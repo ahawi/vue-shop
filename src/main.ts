e6538dc68e6657680f02d6379dbce0c7646ed110
ahawi <nastya.murlla@gmail.com>
2026-03-02 22:17:35 +0300

    fixed main

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,3 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { createYmaps } from 'vue-yandex-maps'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import App from './app/App.vue'
-import router from './app/router'
+import { app } from '@/app'
 
-const app = createApp(App)
-const apiKey = import.meta.env.VITE_MAPS_KEY
-
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
-
-app.use(
-  createYmaps({
-    apikey: apiKey,
-    version: '3.0',
-    lang: 'ru_RU',
-  }),
-)
-
-app.use(pinia)
-app.use(router)
 app.mount('#app')
