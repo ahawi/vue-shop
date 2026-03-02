84a022c7c6c0e8f539e7721a4c517fbeae3390db
ahawi <nastya.murlla@gmail.com>
2026-03-02 22:18:53 +0300

    added constants

---
+++ src/widgets/delivery-step/model/constants.ts
@@ -0,0 +1,14 @@
+export const availableCity = [
+  { id: '1', title: 'Усть-Ижма' },
+  { id: '2', title: 'Москва' },
+  { id: '3', title: 'Санкт-Петербург' },
+  { id: '4', title: 'Новосибирск' },
+  { id: '5', title: 'Екатеринбург' },
+]
+
+export const timeSlots = [
+  { id: '8-14', label: '8:00 - 14:00', available: true, minTime: 8, maxTime: 14 },
+  { id: '14-18', label: '14:00 - 18:00', available: true, minTime: 14, maxTime: 18 },
+  { id: '18-20', label: '18:00 - 20:00', available: true, minTime: 18, maxTime: 20 },
+  { id: '20-22', label: '20:00 - 22:00', available: true, minTime: 20, maxTime: 22 },
+]
