2daffbdcbfdf6b962679af173069c4ecb74fc36c
ahawi <nastya.murlla@gmail.com>
2026-03-02 22:08:50 +0300

    fixed app slice

---
+++ src/app/router/router.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { MAIN_ROUTE } from '@/pages/main'
+import { MAIN_LINK } from '@/shared/config'
+import { CATALOG_ROUTE } from '@/pages/catalog/config'
+import { CATEGORY_ROUTE } from '@/pages/category'
+import { FAVORITES_ROUTE } from '@/pages/favorite'
+import { ORDERS_ROUTE } from '@/pages/orders'
+import { PRODUCT_ROUTE } from '@/pages/product'
+import { CART_ROUTE } from '@/pages/cart'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', redirect: MAIN_LINK },
+    MAIN_ROUTE,
+    CATALOG_ROUTE,
+    CATEGORY_ROUTE,
+    PRODUCT_ROUTE,
+    FAVORITES_ROUTE,
+    ORDERS_ROUTE,
+    CART_ROUTE,
+    { path: '/:catchAll(.*)', redirect: { name: MAIN_LINK.name, params: {} } }
+  ]
+})
+
+export default router
