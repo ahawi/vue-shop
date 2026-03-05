0858c4f026ad94749625a846bad755848d282312
ahawi <nastya.murlla@gmail.com>
2026-03-05 17:15:52 +0300

    added wrapper for layout

--- src/app/layout/Layout.vue
+++ src/app/layout/Layout.vue
@@ -12,19 +12,26 @@ const isMainView = computed(() => route.path === MAIN_LINK.name)
 </script>
 
 <template>
-  <Header />
-  <PromoBar v-if="isMainView" />
-  <main class="main">
-    <RouterView />
-  </main>
-  <Footer />
+  <div class="wrapper">
+    <Header />
+    <PromoBar v-if="isMainView" />
+    <main class="main">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100dvh;
+}
 .main {
   max-width: 1208px;
-  margin: auto;
-  margin-top: 24px;
-  margin-bottom: 80px;
+  width: 100%;
+  margin: 24px auto 80px;
+  flex: 1;
 }
 </style>
